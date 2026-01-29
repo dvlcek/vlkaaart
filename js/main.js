034a04659e3eb75e02e8a8f73d47b6635ac89032
Daniel <dankovlcek@gmail.com>
2026-01-29 14:06:40 +0100

    added responsivnes

--- js/main.js
+++ js/main.js
@@ -8,6 +8,21 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("scrolled");
     }
 });
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("open");
+});
+
+// close menu when clicking a link
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("open");
+  });
+});
 
 // /* ================= HERO SLIDER ================= */
 // const slides = document.querySelectorAll(".hero-slide");
